8a310c9b68ae31a134c78ba7b23fdc6b16e4469c
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-06 21:25:45 -0300

    Parte 3 completa (pulei a 2)

--- ProvaModulo1.js
+++ ProvaModulo1.js
@@ -1,11 +1,38 @@
-// Parte 1
-let valor = prompt("Insira um valor inteiro e positivo")
-console.log(VerificarNumero(valor))
-function VerificarNumero(valorParametro){
-    if(valorParametro % 2 == 0){
+// Parte 3 (CONCLUÍDA)
+let numeroA = prompt("Insira um numero")
+let numeroB = prompt("Insira um numero")
+console.log(VerficarNumero(numeroA, numeroB))
+
+function VerficarNumero(numeroAparametro, numeroBparametro){
+    if(numeroAparametro % 2 == 0 && numeroBparametro > 5){
         return true
     }
     else{
         return false
     }
 }
+
+// Parte 2 (FALTA TERMINAR)
+// let numero = prompt("Insira um número")
+// FazerSoma(numero)
+// let total = 0
+// console.log(total)
+
+// function FazerSoma(valorParametro){
+//     for(let index = 0; index <= valorParametro; index++){
+//         return total = index + 1
+//     }
+// }
+
+
+// // Parte 1 (CONCLUÍDA)
+// let valor = prompt("Insira um valor inteiro e positivo")
+// console.log(VerificarNumero(valor))
+// function VerificarNumero(valorParametro){
+//     if(valorParametro % 2 == 0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
